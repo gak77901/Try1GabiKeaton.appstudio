30c75de05943fc960578664c6903a612ee3ec86f
gak77901 <gak77901@creighton.edu>
2021-09-23 17:55:13 -0500

    While Commit

---
+++ forms/While/While.js
@@ -0,0 +1,16 @@
+/*
+First program - create a program that uses a 'while' loop and outputs each iteration as shown: 
+form named 'while'
+output to console using a template literal: 
+This is loop iteration 1.
+This is loop iteration 2.
+etc....
+*/
+
+let stop = parseInt(prompt("How many times should the loop iterate?"))
+i = 0
+
+while (i < stop) {
+  i = 0 + 1
+  console.log(`This is loop iteration ${i}.`)
+  }
