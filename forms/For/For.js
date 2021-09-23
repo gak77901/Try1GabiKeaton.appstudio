9c2ec13a0c895fc368dc8a767400e6b0da5b0874
gak77901 <gak77901@creighton.edu>
2021-09-23 18:07:54 -0500

    For Commit

---
+++ forms/For/For.js
@@ -0,0 +1,11 @@
+/*
+Second program - create a program on a new form that uses a 'for' loop and an 'array' that outputs each name in the array on its' own line. 
+form named ‘for’
+*/
+
+let array = ["Bob", "Janet", "Tom", "Erin", "Lucy", "Alyssa", "Dustin"]
+
+for (i=0; i < array.length; i++) {
+  console.log(array[i])
+  }
+  
